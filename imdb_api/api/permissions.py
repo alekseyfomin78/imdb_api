4683fed35deca89d13510ba5961a2373db78bb62
alfomin11 <fominal11@mail.ru>
2023-03-10 22:00:20 +0300

    feat(permissions): added permissions for user roles: user, moderator, admin

--- imdb_api/api/permissions.py
+++ imdb_api/api/permissions.py
@@ -1,16 +1,36 @@
-from rest_framework import permissions
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class IsAuthor(permissions.BasePermission):
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return request.user == obj.author
-        return True
-
-
-class IsNotAuth(permissions.BasePermission):
+class IsNotAuth(BasePermission):
 
     def has_permission(self, request, view):
         return not request.user.is_authenticated
 
+
+class AdminOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return request.user.role == 'admin'
+
+
+class IsAdminOrReadOnly(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in ('DELETE', 'PUT', 'PATCH'):
+            if request.user.is_authenticated:
+                return request.user.role == 'admin'
+        else:
+            return True
+
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS or (request.user.is_authenticated and request.user.role == 'admin'):
+            return True
+
+
+class IsAuthorOrModeratorOrAdminOrReadOnly(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        return request.method in SAFE_METHODS or (
+            (request.user == obj.author) or (
+                request.user.role in ('admin', 'moderator')))
